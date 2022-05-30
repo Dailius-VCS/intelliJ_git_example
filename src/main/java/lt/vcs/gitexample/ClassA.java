6a4e51fb8db3bc33f5466ca987a18595a88c507e
Dailius <dailius.rascius@gmail.com>
2022-05-30 21:15:35 +0300

    Instance of ClassA in Main.java

---
+++ src/main/java/lt/vcs/gitexample/ClassA.java
@@ -0,0 +1,8 @@
+package lt.vcs.gitexample;
+
+public class ClassA {
+
+    public void printSomeText(){
+        System.out.println("here is some text");
+    }
+}
--- src/main/java/lt/vcs/gitexample/Main.java
+++ src/main/java/lt/vcs/gitexample/Main.java
@@ -2,6 +2,7 @@ package lt.vcs.gitexample;
 
 public class Main {
     public static void main(String[] args) {
-
+        ClassA classA = new ClassA();
+        classA.printSomeText();
     }
 }
